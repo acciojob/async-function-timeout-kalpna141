df30162af82ba76ee33104557e86ea5a88507967
Kalpna <132220523+kalpna141@users.noreply.github.com>
2023-07-01 17:17:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const inputText = document.getElementById('text');
+const inputDelay = document.getElementById('delay');
+const btn = document.getElementById('btn');
+const outputDiv = document.getElementById('output');
+
+// Function to display the message after a delay
+async function displayMessageWithDelay() {
+  // Get the user input values
+  const text = inputText.value;
+  const delay = parseInt(inputDelay.value);
+
+  // Wait for the specified delay
+  await new Promise(resolve => setTimeout(resolve, delay));
+
+  // Display the message on the webpage
+  outputDiv.textContent = text;
+}
+
+// Event listener for the button click
+btn.addEventListener('click', () => {
+  displayMessageWithDelay();
+});
